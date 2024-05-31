b8f39906693941482dc69f2bab3508c85947eb5f
Chhotu735 <145634071+Chhotu735@users.noreply.github.com>
2024-05-31 07:31:53 +0000

    js_code_2

---
+++ 01_basic/variable.js
@@ -0,0 +1,5 @@
+const accountId=9943473
+ let accountEmail = "chandni124gmail.com"
+  var accountpass="12345"
+  accountcity="jablpur"
+    console.table([accountId,accountEmail,accountpass,accountcity])
